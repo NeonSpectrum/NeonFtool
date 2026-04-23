db4ecde131c11edff7a55c14e48f03ca7e056949
NeonSpectrum <youngskymann@gmail.com>
2026-04-24 06:12:30 +0800

    feat: initialize project structure with single-instance application logic

--- NeonFtool/Program.cs
+++ NeonFtool/Program.cs
@@ -1,19 +1,30 @@
 using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace NeonFtool
 {
     internal static class Program
     {
+        private static Mutex mutex = new Mutex(true, "{8F6F0AC4-B9A1-45fd-A8CF-72F04E6BDE8F}");
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         private static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new NeonFtool());
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new NeonFtool());
+                mutex.ReleaseMutex();
+            }
+            else
+            {
+                MessageBox.Show("An instance of NeonFtool is already running.", "NeonFtool", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
     }
 }
